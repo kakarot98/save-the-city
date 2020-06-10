eea00a34446684dd5c7d7bf6f808ec4693e5b55a
shruti <shrutimundargi98@gmail.com>
2020-06-10 18:23:25 +0530

    meteor spawned randomly

---
+++ Assets/Collision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    //public GameObject meteor, meteorD;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject )
+        {
+            Debug.Log("HIt");
+        }
+    }
+} 
