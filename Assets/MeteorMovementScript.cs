9699098089f3aa8a3395737c8b553d1a15c25ae1
hrushikesh(PC) <hrushikeshmohanty90@gmail.com>
2020-06-10 15:01:08 +0530

    first commit

---
+++ Assets/MeteorMovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMovementScript : MonoBehaviour
+{
+    float moveSpeed;
+    public float offCameraPointVerticalAxis;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+        MoveInVerticalAxis();
+    }
+
+    void MoveInVerticalAxis()
+    {
+        moveSpeed = Random.Range(-4f, -1f);
+        Vector2 temp = transform.position;
+        temp.y += moveSpeed * Time.deltaTime;
+        transform.position = temp;
+
+        if (transform.position.y < offCameraPointVerticalAxis)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/MeteorSpawnerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawnerScript : MonoBehaviour
+{
+    public GameObject meteorPrefab;
+    public float meteorSpawnTimer = 2f;
+    private float currentMeteorSpawnTimer;
+    void Start()
+    {
+        currentMeteorSpawnTimer = meteorSpawnTimer;
+    }
+
+    
+    void Update()
+    {
+        SpawnMeteor();
+    }
+
+    public void SpawnMeteor()
+    {
+        currentMeteorSpawnTimer += Time.deltaTime;
+
+        if (currentMeteorSpawnTimer >= meteorSpawnTimer)
+        {
+           
+            GameObject obstacleBox1 = null;
+            Vector3 parentPos = transform.position;
+            obstacleBox1 = Instantiate(meteorPrefab, new Vector3(-parentPos.x, parentPos.y, parentPos.z), Quaternion.identity);
+            
+
+            currentMeteorSpawnTimer = 0f;
+        }
+
+    }
+}
