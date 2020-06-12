f4703623ca837fa79556349c9c934326ef452252
hrushikesh(PC) <hrushikeshmohanty90@gmail.com>
2020-06-12 15:23:34 +0530

    collider still not working

---
+++ Assets/scripts/CityLifeCycleScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityLifeCycleScript : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    public HealthBarScript healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        //healthBar.SetMaxHealth(maxHealth);
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "meteor") 
+        {
+            Debug.Log("Hit the city");
+            //TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage) 
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+}
